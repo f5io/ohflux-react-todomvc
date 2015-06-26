97aa839493853412f1ff79f7c4a833171e15426b
Joe Harlow <joeharlow@thebioagency.com>
2015-06-26 18:10:14 +0100

    Modify now working

--- src/app.js
+++ src/app.js
@@ -7,22 +7,22 @@ import Immutable from 'immutable';
 
 let isWorking = false;
 
-// TodoStore.log('todoStore');
+TodoStore.log('todoStore');
 
-// TodoStore.onValue(function([ contents, modified ]) {
-// 	let todos = contents.toList().toJS();
-// 	//let filtered = modified.toList().toJS();
-// 	console.log(todos.length);
-// 	if (contents.size === 10 && !isWorking) {
-// 		isWorking = true;
-// 		let temp = contents.takeLast(4).toList().toJS();
-// 		temp.forEach(function(todo) {
-// 			Actions.toggleComplete(todo);
-// 		});
-// 		Actions.setFilter(Constants.FILTER_COMPLETE);
-// 		Actions.setFilter(Constants.FILTER_ALL);
-// 	}
-// });
+TodoStore.onValue(function([ contents, modified ]) {
+	let todos = contents.toList().toJS();
+	let filtered = modified.toList().toJS();
+	console.log(todos.length, filtered.length);
+	if (contents.size === 10 && !isWorking) {
+		isWorking = true;
+		let temp = contents.takeLast(4).toList().toJS();
+		temp.forEach(function(todo) {
+			Actions.toggleComplete(todo);
+		});
+		Actions.setFilter(Constants.FILTER_COMPLETE);
+		Actions.setFilter(Constants.FILTER_ALL);
+	}
+});
 
 for (var i = 0; i < 10; i++) {
 	Actions.create(`hello${i+1}`);
--- src/core/createStore.js
+++ src/core/createStore.js
@@ -8,8 +8,8 @@ function actionsToStreams(actions = {}) {
 	return Object.keys(actions).map(key => actions[key]);
 }
 
-function mutateOrPassthrough([context, actionName, content, ...rest]) {
-	let handler = context[`on${toSentenceCase(actionName)}`];
+function mutateOrPassthrough([context, key, content, ...rest]) {
+	let handler = context[key];
 	return isFunction(handler) && [content, handler(content, ...rest)] || [content, content];
 }
 
@@ -27,100 +27,30 @@ export default function createStore(obj) {
 	let Actions = actionsToStreams(Store.actions)
 		.map(action => Kefir.combine([
 			Kefir.constant(Store),
-			Kefir.constant(action._name),
+			Kefir.constant(`on${toSentenceCase(action._name)}`),
 			action
 		], [ContentPool.sampledBy(action)], (a, b, c, d) => [a, b, d, c]));
 
-	let Mutation = Kefir.merge(Actions)
+	Kefir.merge(Actions)
 		.map(mutateOrPassthrough)
 		.filter(diff)
 		.map(([ currentState, nextState ]) => {
 			ContentPool.plug(Kefir.constant(nextState));
 			return nextState;
 		})
-		.log('mutation');
-		//.toProperty()
-		//.log('stream')
-		// .merge(ContentPool)
-		// .log()
-		// //.map(([ currentState, nextState ]) => ContentPool.plug(Kefir.constant(nextState)))
+		.onValue(() => void 0);
 
-	ContentPool.onValue(val => console.log(val));
+	let Modifier = ContentPool.map(val => val);
+	if (Store.modifier) {
+		let Prop = Store.modifier.toProperty(() => void 0);
+		Modifier = Kefir.combine([
+			Kefir.constant(Store),
+			Kefir.constant(Store.modifier._name),
+			ContentPool,
+			Prop
+		]).map(mutateOrPassthrough);
+	}
 
-	// Stream = Stream.combine()
-	// 	.log('stream');
-
-
-
-
-// 	window.ContentStream = ContentStream;
-
-// <<<<<<< HEAD
-// 	let Stream = Kefir.merge(Actions)
-// 		.skipDuplicates()
-// 		.map(diffAndStore.bind(Store))
-// 		.map(contents => [contents]);
-
-// 	let Modifier;
-// 	// if (Store.modifier) {
-// 	// 	Modifier = Store.modifier.toProperty(() => 0)
-// 	// 		.sampledBy(Stream)
-// 	// 		.map(runHandler.bind(Store, Store.modifier._name));
-
-// 	// 	// Stream = Kefir.combine([Stream, Modifier], (a, b) => b)
-// 	// 	// 	.sampledBy(Stream)
-// 	// 	// 	.map(runHandler.bind(Store, Store.modifier._name))
-// 	// 	// 	.skipDuplicates();
-// 	// }
-
-// 	// Stream = Kefir.combine([Stream, (Modifier || Stream)])
-// 		//.map(([ contents, modified ]) => ({ contents, modified }));
-// =======
-// 	let Actions = actionsToStreams(Store.actions)
-// 		.map(action => Kefir.combine([
-// 			Kefir.constant(Store),
-// 			Kefir.constant(action._name),
-// 			action.flatMap(() => ContentStream.sampledBy(action)),
-// 			action
-// 		]).log());
-
-// 	let Stream = Kefir.merge(Actions).log('Actions')
-// 		.map(mutateOrPassthrough)
-// 		.filter(diff);
-
-// 	Stream.map(([ currentState, nextState ]) => {
-// 			return currentState.merge(nextState);
-// 		})
-// 		.log('merged')
-// 		// .withHandler((emitter, { value }) => {
-// 		// 	ContentStream._emit(value);
-// 		// 	return value;
-// 		// })
-// 		// .skipDuplicates()
-// 		.map(store => store.toJS())
-// 		.log('store');
-// >>>>>>> emitter
-
-// 	Store = inherit(Stream, Store);
-// 	return Store;
-
-// 	// let Actions = Object.keys(Store.actions || {})
-// 	// 	.map(getActionByKey, Store)
-// 	// 	.map(runHandlerAndStore, Store);
-
-// 	// let Stream = Kefir.merge(Actions)
-// 	// 	.map(diffAndStore.bind(Store));
-
-// 	// if (Store.modifier) {
-// 	// 	let Modifier = Store.modifier.toProperty(() => 0);
-// 	// 	Modifier.onValue(() => void 0);
-
-// 	// 	Stream = Kefir.combine([Stream, Modifier], (a, b) => b)
-// 	// 		.sampledBy(Stream)
-// 	// 		.map(runHandler.bind(Store, Store.modifier._name))
-// 	// 		.skipDuplicates();
-// 	// }
-
-// 	// Store = inherit(Stream, Store);
-// 	// return Store;
+	Store = inherit(Modifier, Store);
+	return Store;
 };
