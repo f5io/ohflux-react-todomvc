c97917dfe3b20aff776e2bda0b32ff0d873e40ad
Joe Harlow <joeharlow@thebioagency.com>
2015-07-01 11:46:17 +0100

    Removed old code in app.js

--- src/app.js
+++ src/app.js
@@ -1,37 +1,3 @@
-// import Actions from './actions';
-// import TodoStore from './stores';
-// import Constants from './constants';
-
-// // console.log(Actions);
-// // console.log(TodoStore);
-// // console.log(Constants);
-
-// import Kefir from 'kefir';
-// import Immutable from 'immutable';
-
-// let isWorking = false;
-
-// TodoStore.log('todoStore');
-
-// TodoStore.onValue(function([ contents, modified ]) {
-// 	let todos = contents.toList().toJS();
-// 	let filtered = modified.toList().toJS();
-// 	console.log(todos.length, filtered.length);
-// 	if (contents.size === 10 && !isWorking) {
-// 		isWorking = true;
-// 		let temp = contents.takeLast(4).toList().toJS();
-// 		temp.forEach(function(todo) {
-// 			Actions.toggleComplete(todo);
-// 		});
-// 		Actions.setFilter(Constants.FILTER_COMPLETE);
-// 		Actions.setFilter(Constants.FILTER_ALL);
-// 	}
-// });
-
-// for (var i = 0; i < 10; i++) {
-// 	Actions.create(`hello${i+1}`);
-// }
-
 import React from 'react';
 import { App } from './components';
 
